f26b92037f8073d61259f06bcd9b0f306fd8cb06
kaith05 <ktc24a31c0003@edu.kyoto-tech.ac.jp>
2025-08-20 10:16:26 +0900

    riset

--- tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'status', 'due_date', 'created_at')
-    list_filter = ('status', 'due_date', 'user')
-    search_fields = ('title', 'description')
