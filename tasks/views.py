a238066e02446c9c2f65331f19334a592ed395ff
kaith05 <ktc24a31c0003@edu.kyoto-tech.ac.jp>
2025-08-20 10:41:51 +0900

    タスク管理画面の制作開始

---
+++ tasks/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
